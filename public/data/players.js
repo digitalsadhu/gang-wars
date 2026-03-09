56dc9c817559f5ff179139db8eee734d97b0c024
Richard Walker <richard.walker@m10s.io>
2026-03-09 21:32:34 +0100

    update players

--- public/data/players.js
+++ public/data/players.js
@@ -3,13 +3,14 @@
 
 export default {
   players: [
+    { name: "Mikkel", faction: "The Undergrid Syndicate" },
     { name: "William", faction: "The Undergrid Syndicate" },
-    { name: "Robert", faction: "The Undergrid Syndicate" },
+    { name: "Miseal", faction: "The Undergrid Syndicate" },
     { name: "Fabien", faction: "Static Dominion" },
     { name: "Valentin", faction: "Static Dominion" },
-    { name: "Richard", faction: "Ashfall Covenant" },
-    { name: "Kiran", faction: "Ashfall Covenant" },
-    { name: "Mikkel", faction: "Ashfall Covenant" },
+    { name: "Richard/Kiran", faction: "Static Dominion" },
+    { name: "Rob", faction: "Ashfall Covenant" },
     { name: "Johannes", faction: "Ashfall Covenant" },
+    { name: "Halvor", faction: "Ashfall Covenant" },
   ],
 };
